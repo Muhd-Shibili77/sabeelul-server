faa2a4736e846670a215df78f3433a684df42831
shibili888 <shibilishanu888@gmail.com>
2025-06-04 17:53:01 +0530

    fix:add validation for addmission no

--- src/application/useCase/StudentUseCase.ts
+++ src/application/useCase/StudentUseCase.ts
@@ -68,6 +68,10 @@ export class StudentUseCase {
     if (password.length < 6) {
       throw new Error("Password must be at least 6 characters long.");
     }
+    const admNoExist = await this.studentRepository.findByAdNo(admissionNo)
+    if(admNoExist){
+      throw new Error('Adm No already exist')
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const student = new Student({
@@ -151,9 +155,10 @@ export class StudentUseCase {
     if (password != "" && password.length < 5) {
       throw new Error("Password must be at least 5 characters long.");
     }
-    // const isExist = await this.studentRepository.isExist(email);
-    // if (isExist) {
-    //   throw new Error("Email already exist");
+     const admNoExist = await this.studentRepository.findByAdNo(admissionNo)
+
+    // if(admNoExist?._id != id){
+    //   throw new Error('Adm No already exist')
     // }
 
     const existStudent = await this.studentRepository.findStudentById(id);
